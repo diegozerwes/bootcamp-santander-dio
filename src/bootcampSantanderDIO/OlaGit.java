85bab2830d7e336ee6121e048e7bb413cf917ca4
Diego Zerwes <zerwes.diego@gmail.com>
2023-09-05 16:40:28 -0300

    primeiro commit

---
+++ src/bootcampSantanderDIO/OlaGit.java
@@ -0,0 +1,11 @@
+package bootcampSantanderDIO;
+
+public class OlaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá, Git");
+
+	}
+
+}
---
+++ src/bootcampSantanderDIO/OlaJava.java
@@ -0,0 +1,10 @@
+package bootcampSantanderDIO;
+
+public class OlaJava {
+
+	public static void main(String[] args) {
+		System.out.println("Oi, Java");
+
+	}
+
+}
