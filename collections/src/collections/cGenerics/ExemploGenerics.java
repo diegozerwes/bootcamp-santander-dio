35782cd3dc3967c6a95d38039644a76c6cbceb8a
Diego Zerwes <zerwes.diego@gmail.com>
2023-09-29 17:16:59 -0300

    Inserindo materiais de Collection java

---
+++ collections/src/collections/cGenerics/ExemploGenerics.java
@@ -0,0 +1,18 @@
+package collections.cGenerics;
+
+public class ExemploGenerics<T> {
+	// T representa Type
+	
+	private T t;
+	
+	public void set(T t) {
+		this.t = t;
+		}
+	public T get() {
+		return t;
+	}
+	
+
+	
+
+}
